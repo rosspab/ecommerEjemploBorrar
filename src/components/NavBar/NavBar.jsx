e719b22692d902d49af6b5cd57ace4e7a917d903
Pablo Soto <filmcastudios@gmail.com>
2022-02-07 17:43:16 -0600

    cambios lego navbar realizado

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+
+function Menu(){
+    return(
+        <nav className="background">
+            <ul className="navUl">
+                <li><a href="#">Enlace</a></li>
+                <li><a href="#">Enlace</a></li>
+                <li><a href="#">Enlace</a></li>
+                <li><a href="#">Enlace</a></li>
+                <li><a href="#">Enlace</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu
